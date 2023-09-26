4feb6eb2b27d39230bbf6515555a13edc6524be1
yairsa55 <118170206+yairsa55@users.noreply.github.com>
2023-09-26 10:57:40 +0300

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# this file contain some samples of api keys.
+
+GOOGLE_RECAPTCHA_SITE_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+GOOGLE_RECAPTCHA_SECRET_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+DB_PASSWORD = 'jn25k3kb432b321cxioj412h4oi3u214re432'
+API_TEST = 'testingopiunjv'
+API_PASSWORD = 'testing_name_var'
